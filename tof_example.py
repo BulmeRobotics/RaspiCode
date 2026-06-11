4ad4e4bbab6afb5aaf12e4b9546432a154a5a135
Florian Wiesner <wiesner.florian@gmx.at>
2026-06-11 14:31:53 +0200

    Add tof_example.py

---
+++ tof_example.py
@@ -0,0 +1,14 @@
+import time
+import tof_threshold
+
+# Sensoren initialisieren und Thread starten
+tof_threshold.start()
+
+# Ab hier laeuft der Sensor-Thread im Hintergrund.
+# tof_threshold.state["L"] und ["R"] werden automatisch aktualisiert.
+
+while True:
+    l = tof_threshold.state["L"]
+    r = tof_threshold.state["R"]
+    print(f"Links: {l}  Rechts: {r}")
+    time.sleep(0.1)
